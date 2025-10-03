098bf1c0a677118c7ec20f0dac8c51e3a9dcd0ad
Kayden Leftly <kadeleftly@gmail.com>
2025-10-03 04:45:01 +0000

    Finetuning results

--- data/linuxcommands/prepare.py
+++ data/linuxcommands/prepare.py
@@ -5,6 +5,10 @@ import numpy as np
 import json
 import random
 
+output_dir = 'data/linuxcommands'
+
+print(output_dir)
+
 input_file_path = "data/linuxcommands/commands.json"
 with open(input_file_path, "r", encoding="utf-8") as f:
     json_data = json.load(f)
@@ -24,7 +28,7 @@ test_data = json_data[int(n*0.9):int((n*0.9)+50)]
 with open('data/linuxcommands/test_data.json', mode="w+") as f:
     json.dump(test_data, f, indent=4)
 
-
+print("Read data successfully.")
 # choose amount of data to train on -> we will use 1/4
 n = len(data)
 data = data[:int(n*0.25)]
@@ -43,8 +47,8 @@ print(f"val has {len(val_ids):,} tokens")
 # export to bin files
 train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+train_ids.tofile(os.path.join(output_dir, 'train.bin'))
+val_ids.tofile(os.path.join(output_dir, 'val.bin'))
 
 # train.bin has 301,966 tokens
 # val.bin has 36,059 tokens
